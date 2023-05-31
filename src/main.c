14e1ec40abf62b4145c3fbd6b78cad321a260b50
Roman ASTIE <roman.astie@wanadoo.fr>
2023-05-31 14:42:33 +0200

    commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rastie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 16:42:49 by rastie            #+#    #+#             */
-/*   Updated: 2023/05/21 18:41:59 by rastie           ###   ########.fr       */
+/*   Updated: 2023/05/31 14:42:10 by rastie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 typedef int[2] t_pipe;
@@ -17,8 +17,6 @@ int	main(int ac char **av, char **env)
 {
 	int	infilefd;
 	int	outfilefd;
-	t_pipe	*pipe_list;
-	pid_t	pid;
 
 	if (ac < 5)
 	{
@@ -26,8 +24,11 @@ int	main(int ac char **av, char **env)
 		return (perror(PIPEX_PATTERN), 22);
 	}
 	infilefd = open(av[1], O_RDONLY);
-	if (!ft_strncmp(av[1], "here_doc", 8) && infilefd < 0)
+	if (!ft_strncmp(av[1], "here_doc", 9) && infilefd < 0)
 		infilefd = 0;
+	outfilefd = open(av[ac - 1], O_WRONLY|O_APPEND);
+	if (outfile < 0)
+		return (perror("outfile error"), errno);
 	if (!infile)
 	{
 		if (ac < 6)
@@ -36,10 +37,11 @@ int	main(int ac char **av, char **env)
 			return (perror(HRDOC_PATTERN), 22);
 		}
 		// heredoc
-		return (0);
-	}
-	
+				return (0);
 
+	}
+	if (infile < 0)
+		return (perror("infile error"), errno);
 
 	return (0);
 }
